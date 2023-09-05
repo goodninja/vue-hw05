7c13e7a32c94d5da7ac5219ed6054972ce9754f7
Ivan_Baryshev <obivanivan@mail.ru>
2023-09-05 12:31:36 +0300

    hw05 vue part of task only

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <ProductItem
+        v-for="product in products" 
+        :key="product.id" 
+        :product="product"
+        />
+    </div>
+</template>
+
+<script>
+import ProductItem from '@/components/ProductItem.vue'
+export default {
+    name: 'ProductDetails',
+    components: {
+        ProductItem,
+    },
+    data() {
+        return {
+            products: [
+                {id: 1, name: 'Samsung', price: 15000, available: true},
+                {id: 2, name: 'Apple', price: 35000, available: false},
+                {id: 3, name: 'Nokia', price: 22000, available: true},
+                {id: 4, name: 'Xiaomi', price: 12000, available: false},
+            ]
+        };
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
